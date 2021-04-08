ddaca4922244fc69427b3a5faaa6fe5d28afccc7
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-04-07 22:02:20 -0300

    User insert

--- src/main/java/com/example/projetospringboot/resources/UserResources.java
+++ src/main/java/com/example/projetospringboot/resources/UserResources.java
@@ -5,21 +5,20 @@ import com.example.projetospringboot.entities.User;
 import com.example.projetospringboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
-@RestController
+@RestController //Controlador Rest
 @RequestMapping(value = "/users") //caminho do diretorio (link)
 public class UserResources {
 
     @Autowired
     private UserService service; //UserResources -> UseService
 
-    @GetMapping
+    @GetMapping //utilizado para fazer uma busca
     public ResponseEntity<List<User>> findAll(){
         List<User> lista = service.findAll();
 
@@ -32,4 +31,10 @@ public class UserResources {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping //usado quando for inserir um novo recurso
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 }
