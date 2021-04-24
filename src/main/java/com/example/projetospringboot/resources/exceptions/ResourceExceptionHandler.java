358b69a095f009f1d886b2e6267ef4d46f1bf58a
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-04-24 16:23:14 -0300

    Exception handling - delete

--- src/main/java/com/example/projetospringboot/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/projetospringboot/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.projetospringboot.resources.exceptions;
 
+import com.example.projetospringboot.services.exceptions.DatabaseException;
 import com.example.projetospringboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,20 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+    //EXCESSAO CASO NAO SEJA ENCONTRADA A INFORMACAO
     @ExceptionHandler(ResourceNotFoundException.class) //anotacao serve para retornar um ResponseEntity toda vez que uma excecao do tipo especificado for encontrada
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest requested){
-        String error = "RESOURCE NOT FOUND";
+        String error = "RESOURCE NOT FOUND ";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), requested.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    //EXCESSAO CASO O ATRIBUTO ESTEJA RELACIONADO COM OUTRA TABELA
+    @ExceptionHandler(DatabaseException.class) //anotacao serve para retornar um ResponseEntity toda vez que uma excecao do tipo especificado for encontrada
+    public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest requested){
+        String error = "DataBase Error ";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), requested.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
